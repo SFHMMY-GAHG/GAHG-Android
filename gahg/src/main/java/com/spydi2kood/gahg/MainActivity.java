feb13d3297b632cde0c21091b7d0c27e914ce7a9
spirosbond <spirosbond@hotmail.com>
2014-04-06 22:42:38 +0300

    AsyncTask to implement execute() + center_horizontal for textView + comment out Toast

--- gahg/src/main/java/com/spydi2kood/gahg/MainActivity.java
+++ gahg/src/main/java/com/spydi2kood/gahg/MainActivity.java
@@ -2,6 +2,7 @@ package com.spydi2kood.gahg;
 
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.support.v7.app.ActionBarActivity;
@@ -85,8 +86,8 @@ public class MainActivity extends ActionBarActivity implements View.OnClickListe
 		Log.d(TAG, "onClick");
 		switch (view.getId()) {
 			case R.id.mButton:
-				Toast.makeText(this, "Button Clicked", Toast.LENGTH_LONG).show();
-				execute();
+//				Toast.makeText(this, "Button Clicked", Toast.LENGTH_LONG).show();
+				new Executor().execute();
 		}
 	}
 
@@ -103,5 +104,42 @@ public class MainActivity extends ActionBarActivity implements View.OnClickListe
 		Log.d(TAG, "username: " + mUsername);
 
 		if (!mText.equals("") && !mUsername.equals("")) textView.setText(mUsername.concat(": " + mText));
+	}
+
+	public class Executor extends AsyncTask<Void, Void, String> {
+
+		String mText;
+		String mUsername;
+
+		@Override
+		protected void onPreExecute() {
+			super.onPreExecute();
+			mText = editText.getText().toString();
+			SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
+			mUsername = sharedPreferences.getString("mTextPreference", "");
+			Log.d(TAG, "username: " + mUsername);
+		}
+
+		@Override
+		protected String doInBackground(Void... voids) {
+
+			try {
+				Thread.sleep(2000);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+
+			if (!mText.equals("") && !mUsername.equals("")) {
+				return mUsername.concat(": " + mText);
+			} else {
+				return "";
+			}
+		}
+
+		@Override
+		protected void onPostExecute(String aString) {
+			super.onPostExecute(aString);
+			if (!aString.equals("")) textView.setText(aString);
+		}
 	}
 }
