bced0675e8f7d6e998add610bfaca1320fd3ba7e
spirosbond <spirosbond@hotmail.com>
2014-04-06 22:14:31 +0300

    addPreferenceFromResource for settings

--- gahg/src/main/java/com/spydi2kood/gahg/SettingsActivity.java
+++ gahg/src/main/java/com/spydi2kood/gahg/SettingsActivity.java
@@ -6,10 +6,10 @@ import android.preference.PreferenceActivity;
 /**
  * Created by spiros on 4/6/14.
  */
-public class SettingsActivity extends PreferenceActivity{
+public class SettingsActivity extends PreferenceActivity {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-
+		addPreferencesFromResource(R.xml.settings_main);
 	}
 }
